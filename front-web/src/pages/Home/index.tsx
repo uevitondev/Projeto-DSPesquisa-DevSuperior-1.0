13fb2409d133e57ce539eb4fa0e4a464b66521fa
Ueviton Santos <uevitonsantos@outlook.com>
2020-09-17 18:07:18 -0300

    Criando pagina Home

---
+++ front-web/src/pages/Home/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./style.css";
+import { ReactComponent as ArrowIcon } from "../../assets/arrow.svg";
+import { ReactComponent as GamerImage } from "../../assets/gamer.svg";
+
+const Home = () => (
+    <div className="home-container">
+        <div className="home-text">
+            <h1 className="home-text-title">
+                Quais Jogos a galera gosta mais?
+            </h1>
+            <h3 className="home-text-subtitle">
+                Clique no botão abaixo e saiba quais os jogos que os games estão escolhendo!
+            </h3>
+            <div className="home-actions">
+                <button className="home-btn">
+                    QUERO SABER QUAIS SÃO!
+                </button>
+                <div className="home-btn-icon">
+                    <ArrowIcon />
+                </div>
+            </div>
+        </div>
+        <div>
+            <GamerImage className="home-image" />
+        </div>
+    </div>
+);
+
+export default Home;
