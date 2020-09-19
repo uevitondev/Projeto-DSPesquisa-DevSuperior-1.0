ea39664784a1b4b024ba52b63a8f3ced22366cfa
Ueviton Santos <uevitonsantos@outlook.com>
2020-09-19 17:12:12 -0300

    Configurando front-web para produção

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -21,7 +21,7 @@ const initialPieData = {
     series: []
 }
 
-const BASE_URL = "http://localhost:8080"
+const BASE_URL = "https://sds1-ueviton.herokuapp.com";
 
 const Charts = () => {
     const [barchartData, setBarChartData] = useState<BarChartData[]>([]);
